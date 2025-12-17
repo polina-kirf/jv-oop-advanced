50edfcce07c0ce808e0060444f52587bae3e37a8
Polina Kirf <polinakirf06@gmail.com>
2025-12-17 18:15:20 +0200

    Refactor Figure: move getArea and getParameters to separate interfaces

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,19 +1,15 @@
 package core.basesyntax;
 
-public abstract class Figure {
+public abstract class Figure implements AreaCalculable, Parameterizable {
     protected String color;
 
     public Figure(String color) {
         this.color = color;
     }
 
-    public abstract double getArea();
-
-    public abstract String getParameters();
-
     public void draw() {
-        System.out.println("Figure: " + this.getClass().getSimpleName()
-        + ", area: " + String.format("%.2f", getArea()) + " sq.units, " + getParameters()
+        System.out.println("Figure: " + this.getClass().getSimpleName() + ", area: "
+                + String.format("%.2f", getArea()) + " sq. units, " + getParameters()
                 + ", color: " + color);
     }
 }
